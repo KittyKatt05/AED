8519f6b9b3e073f94b3102548e7c1d53cfd9fb28
Catarina <1201338@isep.ipp.pt>
2021-11-27 21:24:28 +0000

    first commit

---
+++ cmake-build-debug/CMakeFiles/trabalho1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trabalho1.dir/main.cpp.obj"
+  "libtrabalho1.dll.a"
+  "trabalho1.exe"
+  "trabalho1.exe.manifest"
+  "trabalho1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trabalho1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
