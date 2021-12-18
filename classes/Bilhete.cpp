0795d7431ce39db660c6dfa0fcc146c653928fd9
Catarina <1201338@isep.ipp.pt>
2021-12-18 22:08:27 +0000

    Documentação

--- classes/Bilhete.cpp
+++ classes/Bilhete.cpp
@@ -7,23 +7,44 @@
 
 using namespace std;
 
+/**
+ * Cria um objeto do tipo Bilhete com os atributos passados por parâmetro.
+ * @param passageiro
+ * @param voo
+ */
 Bilhete::Bilhete(Passageiro passageiro, Voo voo) : voo(nullptr, Data(), nullptr, nullptr, nullptr, Passageiro(), Hora()) {
     this->passageiro = passageiro;
     this->voo = voo;
 }
 
+/**
+ * Permite aceder ao passageiro que possui o bilhete.
+ * @return passageiro
+ */
 Passageiro Bilhete::getPassageiro() {
     return this->passageiro;
 }
 
+/**
+ * Permite aceder ao voo do bilhete
+ * @return voo
+ */
 Voo Bilhete::getVoo() {
     return this->voo;
 }
 
+/**
+ * Permite modificar o passageiro do bilhete.
+ * @param passageiro1
+ */
 void Bilhete::setPassageiro(Passageiro passageiro1) {
     this->passageiro = passageiro1;
 }
 
+/**
+ * Permite modificar o voo do bilhete.
+ * @param voo1
+ */
 void Bilhete::setVoo(Voo voo1) {
     this->voo = voo1;
 }
